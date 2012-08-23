5aaed1dac9d504c0417341070f2f43e9002ced91
unknown <tialiu@tialiu-WS.cisco.com>
2012-08-22 18:11:59 -0700

    Initial version

---
+++ src/Percolation.java
@@ -0,0 +1,16 @@
+
+public class Percolation {
+	
+	
+	public Percolation(int N)
+	{
+		
+	}
+	
+	// open site (row i, column j) if it is not already
+	public void open(int i, int j)
+	{
+		
+	}
+
+}

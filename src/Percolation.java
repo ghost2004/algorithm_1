4fd1b07408780e71cc7545850793b5ff94ae6197
Tiangang Liu <liutg@hotmail.com>
2012-08-25 00:16:43 -0700

    Fix backwash issue

--- src/Percolation.java
+++ src/Percolation.java
@@ -7,10 +7,11 @@ public class Percolation {
 
     private boolean[][] matrix;
     private WeightedQuickUnionUF union;
+    private WeightedQuickUnionUF union2;
     private int virtualTop;
-    // private int virtualBottom;
+    private int virtualBottom;
     private int side;
-    private boolean percolatesFlag;
+
 
     // create N-by-N grid, with all sites blocked
     public Percolation(int N)
@@ -24,20 +25,19 @@ public class Percolation {
                 matrix[i][j] = false;
         }
         union = new WeightedQuickUnionUF(N * N + 2);
-        
+        union2 = new WeightedQuickUnionUF(N * N + 2);
         virtualTop = N * N;
-        // virtualBottom = N * N + 1;
+        virtualBottom = N * N + 1;
 
-        // j = N * (N - 1);
+        j = N * (N - 1);
         
         for (i = 0; i < N; i++) {
             union.union(i, virtualTop);
-            // union.union(j + i, virtualBottom);
+            union2.union(i, virtualTop);
+            union.union(j + i, virtualBottom);
         }
         side = N;
-        
-        percolatesFlag = false;
-        
+      
     }
     
     private int getIndex(int i, int j)
@@ -56,24 +56,26 @@ public class Percolation {
         int y = j-1;
         matrix[x][y] = true;
         // left
-        if (x > 0 && matrix[x-1][y])
+        if (x > 0 && matrix[x-1][y]) {
             union.union(getIndex(x-1, y), getIndex(x, y));
-        // right
-        if (x < side-1 && matrix[x+1][y])
-            union.union(getIndex(x+1, y), getIndex(x, y));
-        // up
-        if (y > 0 && matrix[x][y-1])
-            union.union(getIndex(x, y-1), getIndex(x, y));
-        // down
-        if (y < side-1 && matrix[x][y+1])
-            union.union(getIndex(x, y+1), getIndex(x, y));
-        
-        // if it is the site at bottom line, check the grid percolates Flag
-        if (x == side-1) {
-            if (isFull(i, j))
-                percolatesFlag = true;
+            union2.union(getIndex(x-1, y), getIndex(x, y));
         }
-            
+        // right
+        if (x < side-1 && matrix[x+1][y]) {
+            union.union(getIndex(x+1, y), getIndex(x, y));
+            union2.union(getIndex(x+1, y), getIndex(x, y));
+        }
+        // up
+        if (y > 0 && matrix[x][y-1]) {
+            union.union(getIndex(x, y-1), getIndex(x, y));
+            union2.union(getIndex(x, y-1), getIndex(x, y));
+        }
+        // down
+        if (y < side-1 && matrix[x][y+1]) {
+            union.union(getIndex(x, y+1), getIndex(x, y));
+            union2.union(getIndex(x, y+1), getIndex(x, y));
+        }
+         
     }
     
     // is site (row i, column j) open?
@@ -94,13 +96,17 @@ public class Percolation {
         if (i > side || j > side 
                 || i <= 0 || j <= 0)
             throw new java.lang.IndexOutOfBoundsException();        
-        return (isOpen(i, j) && union.connected(getIndex(i-1, j-1), virtualTop));
+        return (isOpen(i, j) && union2.connected(getIndex(i-1, j-1), virtualTop));
     }
     
     
     public boolean percolates()
     {
-        return percolatesFlag;
+        if (side == 1)
+            return matrix[0][0];
+        
+        return (union.connected(virtualTop, virtualBottom));
+        
     }
 
 }

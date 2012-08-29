7ddede721113e06a0cd16eada921e580d22a86ed
Tiangang Liu <liutg@hotmail.com>
2012-08-29 16:02:27 -0700

    initial

---
+++ src/Deque.java
@@ -0,0 +1,16 @@
+
+public class Deque<Item> implements Iterable<Item> { 
+    
+    // construct an empty deque
+    public Deque(){
+        
+    }
+    
+    // is the deque empty?
+    public boolean isEmpty() {
+        
+    }
+    
+    // return the number of items on the deque
+    
+}

26dd0879063539487e40f79a64fb5cb670e1ef32
Tiangang Liu <liutg@hotmail.com>
2012-08-31 23:38:08 -0700

    Random queue implementation

--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -1,17 +1,22 @@
+import java.util.Arrays;
+import java.util.Iterator;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-    private final int initialSize = 10;
-    private Object[] elementData;
+    private final int queueNumber = 10;
+    private Item[] elementData;
     private int arraySize;
     private int objSize;
+
+
    
     // construct an empty randomized queue
     
     public RandomizedQueue() {
-        this.arraySize = initialSize;
+        this.arraySize = queueNumber;
         this.objSize = 0;
-        this.elementData = new Object [initialSize];
+        this.elementData = (Item[]) new Object[queueNumber];
+
     }
     
     // is the queue empty?
@@ -22,6 +27,96 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     // return the number of items on the queue
     public int size() {
         return objSize;
+    }
+    
+    // resize array to new capacity
+    private void resize(int capacity)
+    {
+        Item[] copy = (Item[]) new Object[capacity];
+        copy = Arrays.copyOf(elementData, objSize);
+        elementData = copy;
+        arraySize = capacity;
+    }
+    
+    private void exchange(int a, int b)
+    {
+        Item temp = elementData[a];
+        elementData[a] = elementData[b];
+        elementData[b] = temp;
+    }
+    
+    
+    // add the item
+    public void enqueue(Item item) {
+        if (objSize == arraySize) {
+            resize(arraySize*2);
+        }
+        
+        elementData[objSize++] = item;
+
+    }
+    
+    // delete and return a random item
+    public Item dequeue() {
+        if (objSize == 0)
+            throw new java.util.NoSuchElementException();
+        int last = objSize-1;
+        
+        if (last != 0)
+        {
+            int idx = StdRandom.uniform(objSize);
+            
+            if (idx !=  last)
+                exchange(idx, last);
+        }
+
+        
+        Item out = elementData[last];
+        elementData[last] = null;
+        objSize--;
+        if (objSize < arraySize/4)
+            resize(arraySize/2);
+        return out;
+    }
+    
+    // return (but do not delete) a random item
+    public Item sample() {
+        if (objSize == 0)
+            throw new java.util.NoSuchElementException();
+        int idx = StdRandom.uniform(objSize);
+        return elementData[idx];
+    }
+    
+    private class RandomIterator implements Iterator<Item> {
+        private int[] sequence;
+        private int seqPointer;
+        public RandomIterator() {
+            sequence = new int [objSize];
+            for (int i = 0; i < objSize; i++)
+                sequence[i] = i;
+            StdRandom.shuffle(sequence);
+            seqPointer = 0;
+            
+        }
+        public void remove() {
+            throw new java.lang.UnsupportedOperationException();
+        }
+        public boolean hasNext() {
+            return seqPointer < objSize;
+        }
+        public Item next() {
+            if (seqPointer >= objSize)
+                throw new java.util.NoSuchElementException();
+            Item out = elementData[sequence[seqPointer]];
+            seqPointer++;
+            return out;
+        }
+    
+    }
+    
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {
+        return new RandomIterator();
     }
     
 }
